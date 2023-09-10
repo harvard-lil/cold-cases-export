734f25c55bd48e154864fd29b1ff484896bb5c79
Michael Della Bitta <mdellabitta@tutanota.com>
2023-09-10 13:37:04 -0400

    Figured out my dataframe api problems!

--- courtlistener_export/export.py
+++ courtlistener_export/export.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.types import IntegerType, DateType, ArrayType, StringType, Row
+from pyspark.sql.types import IntegerType, DateType
 from pyspark.sql.functions import (
     coalesce,
     when,
@@ -52,6 +52,7 @@ def get_opinions(spark: SparkSession, path: str) -> DataFrame:
         "sha1",
         "local_path",
         "extracted_by_ocr",
+        "cluster_id",
     ]
     drop_cols.extend(text_col_priority)
     return (
@@ -62,7 +63,7 @@ def get_opinions(spark: SparkSession, path: str) -> DataFrame:
         .withColumn("page_count", col("page_count").cast(IntegerType()))
         .withColumn("ocr", when(col("extracted_by_ocr") == "t", True).otherwise(False))
         .withColumn("per_curiam", when(col("per_curiam") == "t", True).otherwise(False))
-        .withColumn("cluster_id", col("cluster_id").cast(IntegerType()))
+        .withColumn("opinion_cluster_id", col("cluster_id").cast(IntegerType()))
         .withColumn("author_id", col("author_id").cast(IntegerType()))
         .withColumn("opinion_id", col("id").cast(IntegerType()))
         .drop(*drop_cols)
@@ -110,48 +111,16 @@ def get_citations(spark: SparkSession, path: str) -> DataFrame:
         "volume",
         "reporter",
         "page",
+        "cluster_id",
         "type",  # might be needed. no current lookup table provided.
     ]
     return (
         spark.read.parquet(path)
         .alias("c")
         .withColumn("citation_text", concat_ws(" ", "volume", "reporter", "page"))
-        .withColumn("cluster_id", col("cluster_id").cast(IntegerType()))
+        .withColumn("citation_cluster_id", col("cluster_id").cast(IntegerType()))
         .withColumn("id", col("id").cast(IntegerType()))
         .drop(*drop_cols)
-    )
-
-
-def rdd_group(
-    citations: DataFrame, opinions: DataFrame, opinion_clusters: DataFrame
-) -> DataFrame:
-    citations_keyed = citations.rdd.map(lambda x: (x.cluster_id, x.citation_text))
-    opinions_keyed = opinions.rdd.map(lambda x: (x.cluster_id, x))
-
-    grouped = opinion_clusters.rdd.map(lambda x: (x.id, x)).groupWith(
-        opinions_keyed, citations_keyed
-    )
-
-    schema = opinion_clusters.schema
-    schema.add("opinions", ArrayType(opinions.schema))
-    schema.add("citations", ArrayType(StringType()))
-
-    def reparent_opinions(row):
-        key, values = row
-        if not list(values[0]):
-            return []  # this grouping had no cluster
-        cluster = list(values[0])[0]  # list(list(pair[0])[0][0])[0]
-        opinions = list(values[1])  # list(list(pair[0])[0][1])
-        citations = list(values[2])  # list(pair[1])
-        dict = cluster.asDict()
-        dict["opinions"] = opinions
-        dict["citations"] = citations
-        return [Row(**dict)]
-
-    return (
-        grouped.flatMap(lambda x: reparent_opinions(x))
-        .toDF(schema)
-        .drop("opinions.cluster_id")
     )
 
 
@@ -166,17 +135,23 @@ def group(
     api, but I didn't get around to figuring it out.
     """
 
-    citations_arrays = citations.groupby(citations.cluster_id).agg(
+    citations_arrays = citations.groupby(citations.citation_cluster_id).agg(
         collect_list(struct(*[col(c).alias(c) for c in citations.columns]))
     )
 
-    opinions_arrays = opinions.groupby(opinions.cluster_id).agg(
+    opinions_arrays = opinions.groupby(opinions.opinion_cluster_id).agg(
         collect_list(struct(*[col(c).alias(c) for c in opinions.columns]))
     )
 
     return opinion_clusters.join(
-        citations_arrays, opinion_clusters.id == citations_arrays.cluster_id, "left"
-    ).join(opinions_arrays, opinion_clusters.id == citations_arrays.cluster_id, "left")
+        citations_arrays,
+        opinion_clusters.id == citations_arrays.citation_cluster_id,
+        "left",
+    ).join(
+        opinions_arrays,
+        opinion_clusters.id == opinions_arrays.opinion_cluster_id,
+        "left",
+    )
 
 
 def find_latest(directory: str, prefix: str, extension: str) -> str:
@@ -194,7 +169,11 @@ def find_latest(directory: str, prefix: str, extension: str) -> str:
 
 
 def run(data_dir: str) -> None:
-    spark = SparkSession.builder.appName("courtlistener-export").getOrCreate()
+    spark = (
+        SparkSession.builder.config("spark.sql.autoBroadcastJoinThreshold", "0")
+        .appName("courtlistener-export")
+        .getOrCreate()
+    )
 
     if data_dir.endswith("/"):
         data_dir = data_dir[0:-1]
@@ -217,7 +196,8 @@ def run(data_dir: str) -> None:
     opinions = get_opinions(spark, latest_opinions_parquet)
     opinion_clusters = get_opinion_clusters(spark, latest_clusters_parquet)
 
-    reparented = rdd_group(citations, opinions, opinion_clusters)
+    reparented = group(citations, opinions, opinion_clusters)
+    reparented.explain(extended=True)
     reparented.write.parquet(data_dir + "/courtlistener.parquet")
 
     spark.read.parquet(data_dir + "/courtlistener.parquet").write.json(
