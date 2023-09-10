acf554c052db040fb4aab85316efa9d0ec1cede3
Michael Della Bitta <mdellabitta@tutanota.com>
2023-09-10 13:58:15 -0400

    Added courts and dockets. Made script more DRY.

--- courtlistener_export/download.py
+++ courtlistener_export/download.py
@@ -5,6 +5,30 @@ import xmltodict
 
 STORAGE_URL = "https://com-courtlistener-storage.s3-us-west-2.amazonaws.com"
 
+
+def latest(files: list[str]) -> str:
+    files.sort()
+    return files[-1]
+
+
+def download(filename: str) -> None:
+    print(f"Downloading {filename}...")
+    outfile = f"data/{filename}"
+
+    if os.path.exists(outfile):
+        print(f"{filename} already exists.")
+        print()
+        return
+
+    print()
+    with open(f"data/{filename}", "wb") as dl_file:
+        with requests.get(
+            f"{STORAGE_URL}/bulk-data/{filename}", stream=True
+        ) as dl_response:
+            for chunk in dl_response.iter_content(chunk_size=8192):
+                dl_file.write(chunk)
+
+
 if __name__ == "__main__":
     response = requests.get(STORAGE_URL + "/?delimiter=/&prefix=bulk-data/")
     listing = xmltodict.parse(response.text)
@@ -13,46 +37,19 @@ if __name__ == "__main__":
         print("Truncated result. Script needs to be rebuilt.", file=sys.stderr)
         sys.exit(1)
 
-    citations = []
-    opinions = []
-    opinion_clusters = []
+    data_types = ["citations", "opinions", "opinion-clusters", "dockets", "courts"]
+
+    type_listings = {}
+
+    for t in data_types:
+        type_listings[t] = []
 
     for file in listing["ListBucketResult"]["Contents"]:
         key = file["Key"]
         s3_filename = key.split("/")[-1]
+        for t in data_types:
+            if s3_filename.startswith(t):
+                type_listings[t].append(s3_filename)
 
-        if s3_filename.startswith("citations"):
-            citations.append(s3_filename)
-
-        elif s3_filename.startswith("opinion-clusters"):
-            opinion_clusters.append(s3_filename)
-
-        elif s3_filename.startswith("opinions"):
-            opinions.append(s3_filename)
-
-    def latest(files: list[str]) -> str:
-        files.sort()
-        return files[-1]
-
-    latest_citations = latest(citations)
-    latest_opinions = latest(opinions)
-    latest_opinion_clusters = latest(opinion_clusters)
-
-    def download(filename: str) -> None:
-        print(f"Downloading {filename}...")
-        outfile = f"data/{filename}"
-
-        if os.path.exists(outfile):
-            print(f"{filename} already exists.")
-            return
-
-        with open(f"data/{filename}", "wb") as dl_file:
-            with requests.get(
-                f"{STORAGE_URL}/bulk-data/{filename}", stream=True
-            ) as dl_response:
-                for chunk in dl_response.iter_content(chunk_size=8192):
-                    dl_file.write(chunk)
-
-    download(latest_citations)
-    download(latest_opinion_clusters)
-    download(latest_opinions)
+    for t in data_types:
+        download(latest(type_listings[t]))
